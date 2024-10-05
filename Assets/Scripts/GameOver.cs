ac181a6ee9441cd277ae22ad660a44903b5eeca9
Sameer Bashir <ssjsameer9000@gmail.com>
2024-10-05 17:46:52 -0400

    Fixed Buttons

--- Assets/Scripts/GameOver.cs
+++ Assets/Scripts/GameOver.cs
@@ -1,18 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameOver : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public void ChangeScene(int sceneID)
     {
-        
+        Debug.Log(sceneID);
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
